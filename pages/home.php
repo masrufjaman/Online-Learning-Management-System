519ef1ead115e198ff28304ce3ff8f42028cb505
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-07-21 23:46:03 +0600

    create homepage

---
+++ pages/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brainstormy</title>
+    <style>
+        <?php include "style.css" ?>
+    </style>
+</head>
+
+<body>
+    <section id="header">
+        <div class="container">
+            <img src="" alt="logo" class="logo">
+        </div>
+    </section>
+</body>
+
+</html>
