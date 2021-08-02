3a7afbbab6eecd03ed62424c42ce28e9ee2856a7
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-03 01:30:39 +0600

    changeed file and folder structure

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php
-include("./pages/home.php");
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brainstormy</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <section id="header">
+        <div class="container">
+            <img src="" alt="logo" class="logo">
+        </div>
+    </section>
+</body>
+
+</html>
