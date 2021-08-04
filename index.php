d4a0e48408ac6ab6a5ef3d136bd0e5ef7fbc6bca
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-04 13:56:26 +0600

    header-text

--- index.php
+++ index.php
@@ -2,17 +2,25 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Brainstormy</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css" />
 </head>
 
 <body>
     <section id="header">
         <div class="container">
-            <img src="" alt="logo" class="logo">
+            <img src="/assets/img/logo.png" alt="logo" class="logo" />
+            <div class="header-text">
+                <h1>The purpose is to<br />tech, bring learning to people</h1>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio odio
+                    laudantium sequi sapiente ratione maiores ex possimus quibusdam
+                    maxime. Distinctio mollitia illum nesciunt cupiditate tenetur.
+                </p>
+            </div>
         </div>
     </section>
 </body>
