a34edb9ca51c7cc57b9c66b307c6e22256f4a7d1
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-07 22:55:48 +0600

    line design

--- index.php
+++ index.php
@@ -15,11 +15,18 @@
             <img src="/assets/img/logo.png" alt="logo" class="logo" />
             <div class="header-text">
                 <h1>The purpose is to<br />tech, bring learning to people</h1>
+                <span class="square"></span>
                 <p>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio odio
                     laudantium sequi sapiente ratione maiores ex possimus quibusdam
                     maxime. Distinctio mollitia illum nesciunt cupiditate tenetur.
                 </p>
+                <button class="common-btn">Read More</button>
+                <div class="line">
+                    <span class="line-1"></span><br />
+                    <span class="line-2"></span><br />
+                    <span class="line-3"></span>
+                </div>
             </div>
         </div>
     </section>
