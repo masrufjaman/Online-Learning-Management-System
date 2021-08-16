2b653d4d4b2677c953853b96e3def1b577140981
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-17 01:11:31 +0600

    created admin dashboard

---
+++ templates/admin/dashboard.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Brainstormy</title>
+  <!-- Font Awsome Link -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous" />
+  <!-- CSS Link -->
+  <link rel="stylesheet" href="/assets/css/adminDashboard.css" />
+</head>
+
+<body>
+  <div class="container">
+    <!-- Side Dashboard Pane Start -->
+    <div class="navigation">
+      <ul>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-apple" aria-hidden="true"></i></span>
+            <span class="title">
+              <h2>Brainstormy</h2>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-home" aria-hidden="true"></i></span>
+            <span class="title">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+            <span class="title">Faculties</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-users" aria-hidden="true"></i></span>
+            <span class="title">Students</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-comment" aria-hidden="true"></i></span>
+            <span class="title">Message</span></a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-question-circle" aria-hidden="true"></i></span>
+            <span class="title">Help</span></a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-cog" aria-hidden="true"></i></span>
+            <span class="title">Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-lock" aria-hidden="true"></i></span>
+            <span class="title">Password</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="icon"><i class="fa fa-sign-out" aria-hidden="true"></i></span>
+            <span class="title">Sign Out</span></a>
+        </li>
+      </ul>
+    </div>
+    <!-- Side Dashboard Pane End -->
+  </div>
+</body>
+
+</html>
