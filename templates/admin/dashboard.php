bfbda22c04a284f08d0092c09e28dec8337e55b0
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-17 16:53:45 +0600

    modify directory

--- templates/admin/dashboard.php
+++ templates/admin/dashboard.php
@@ -9,7 +9,7 @@
   <!-- Font Awsome Link -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous" />
   <!-- CSS Link -->
-  <link rel="stylesheet" href="./styles/adminDashboard.css" />
+  <link rel="stylesheet" href="styles/admin.css" />
 </head>
 
 <body>
