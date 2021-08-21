354161a2090583a7edea47d2df0b94ebf7b7dd72
shadia007 <66166732+shadia007@users.noreply.github.com>
2021-08-21 21:13:41 +0600

    Registrations with login part done with sql

--- templates/home.php
+++ templates/home.php
@@ -50,6 +50,7 @@
             <li><a href="#courses">COURSES</a></li>
             <li><a href="#offer">OFFER</a></li>
             <li><a href="#contact">CONTACT</a></li>
+            <li><a href="templates/Registrations/index.php">REGISTRATIONS</a></li>
             <li><a href="templates/admin/dashboard.php">ADMIN PANEL</a></li>
             <li><a href="templates/students/dashboard.php">CLASSROOM</a></li>
             <li><a href="templates/lecturers/dashboard.php">TEACHER</a></li>
