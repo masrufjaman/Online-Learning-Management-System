5fe5254d2567917a35171a7c9e2eb50080a245a3
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-29 17:01:59 +0600

    Add student functionality

--- templates/admin/add_modal.php
+++ templates/admin/add_modal.php
@@ -4,83 +4,59 @@
         <div class="close">+</div>
         <!-- For adding new student -->
         <h1>Add New Student</h1>
-        <!-- without <enctype="multipart/form-data"> photo doesn't upload -->
-        <form action="studentsView.php" method="post" enctype="multipart/form-data">
+        <form action="add_modal.php" method="post">
             <div class="user-details">
                 <div class="input-box">
-                    <span class="details">Full Name</span>
-                    <input type="text" name="FullName" placeholder="Enter the name" required>
-                </div>
-                <div class="input-box">
                     <span class="details">Username</span>
-                    <input type="text" name="" placeholder="Enter the username" required>
+                    <input type="text" name="username" placeholder="Enter the username" required>
                 </div>
                 <div class="input-box">
                     <span class="details">Email</span>
-                    <input type="text" name="" placeholder="Enter the email" required>
-                </div>
-                <div class="input-box">
-                    <span class="details">Phone Number</span>
-                    <input type="text" name="ParentsContact" placeholder="Enter the number" required>
+                    <input type="email" name="email" placeholder="Enter the email" required>
                 </div>
                 <div class="input-box">
                     <span class="details">Password</span>
-                    <input type="text" name="" placeholder="Enter the password" required>
+                    <input type="password" name="password" placeholder="Enter the password" required>
                 </div>
-                <div class="input-box">
-                    <span class="details">Confirm Password</span>
-                    <input type="text" name="" placeholder="Confirm the password" required>
-                </div>
-            </div>
-            <div class="gender-details">
-                <input type="radio" name="gender" id="dot-1">
-                <input type="radio" name="gender" id="dot-2">
-                <input type="radio" name="gender" id="dot-3">
-                <span class="gender-title">Gender</span>
-                <div class="category">
-                    <label for="dot-1">
-                        <span class="dot one"></span>
-                        <span class="gender">Male</span>
-                    </label>
-                    <label for="dot-2">
-                        <span class="dot two"></span>
-                        <span class="gender">Female</span>
-                    </label>
-                    <label for="dot-3">
-                        <span class="dot three"></span>
-                        <span class="gender">Prefer not to say</span>
-                    </label>
-                </div>
-            </div>
-            <div class="uploadImage">
-                <span class="imageTitle">Image:</span>
-                <input type="file" name="simg" required>
             </div>
             <div class="button">
                 <input type="submit" name="submit" value="Register">
             </div>
         </form>
+
+        <!-- PHP Code -->
         <?php
         if (isset($_POST['submit'])) {
-            include('/xampp/htdocs/brainstormy/libs/db_connect.php');
 
-            $FullName = $_POST['FullName'];
-            $ParentsContact = $_POST['ParentsContact'];
-            $imageName = $_FILES['simg']['name'];
-            $tempName = $_FILES['simg']['tmp_name'];
+            include('../../libs/db_connect.php');
 
-            move_uploaded_file($tempName, "/xampp/htdocs/brainstormy/dataimg/$imageName");
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = md5($_POST['password']);
+            $role = 'student';
 
-            $qry = "INSERT INTO `student_details`(`FullName`, `ParentsContact`, `Photo`) VALUES ('$FullName', '$ParentsContact', '$imageName')";
+            $sql = "SELECT * FROM users WHERE email='$email'";
+            $result = mysqli_query($con, $sql);
+            if (!$result->num_rows > 0) {
+                $sql = "INSERT INTO users (username, email, password, role)
+					VALUES ('$username', '$email', '$password', '$role')";
+                $result = mysqli_query($con, $sql);
+                if ($result) {
+                    echo "<script>alert('Wow! User Registration Completed.')</script>";
 
-            $run = mysqli_query($con, $qry);
+                    // Insert ID of users table
+                    $uid = $con->insert_id;
 
-            if ($run == true) {
-        ?>
-                <script>
-                    alert('Data inserted Succesfully');
-                </script>
-        <?php
+                    $sql = "INSERT INTO student_details (uid)
+					VALUES ('$uid')";
+                    $result = mysqli_query($con, $sql);
+
+                    header("Location: ./studentsView.php");
+                } else {
+                    echo "<script>alert('Woops! Something Wrong Went.')</script>";
+                }
+            } else {
+                echo "<script>alert('Woops! Email Already Exists.')</script>";
             }
         }
         ?>
